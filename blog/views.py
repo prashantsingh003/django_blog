fb521e0118b3c3ace7ec6edf742080f600a35f69
Prashant <prashant@inuvest.tech>
2022-12-30 12:16:54 +0530

    models integrations

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 # Create your views here.
 
-posts=[
-    {
-        'author':'John Doe',
-        'title':'Blog Post 1',
-        'content':'First post content',
-        'date_posted':'August 27, 2012'
-    },
-    {
-        'author':'Titania Doe',
-        'title':'Blog Post 2',
-        'content':'Second post content',
-        'date_posted':'July 05, 2022'
-    }
-]
 
 def home(request):
     context={
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blog/home.html',context=context)
 
