998b6346c8e27ee6dc978306b85130b7c49948b6
Prashant Singh <53073860+prashantsingh003@users.noreply.github.com>
2023-03-10 14:43:07 -0800

    single post page

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 # Create your views here.
 
-
+# All post with function view
 def home(request):
     context={
         'posts':Post.objects.all()
     }
     return render(request,'blog/home.html',context=context)
+
+# All post with list view
+class PostListView(ListView):
+    model=Post
+    template_name='blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name='posts'
+    ordering=['-date_posted']
+
+# Single post with detail view
+class PostDetailView(DetailView):
+    model=Post
 
 def about(request):
     context={
